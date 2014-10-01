bc56807c175e51db9c737e295e8c1393c4bbf4ec
LNACHT <LNACHT@LFR002637.corp.capgemini.com>
2014-10-01 11:32:17 +0200

    creation de la view agency

---
+++ ProjectsTrainning/com.sogeti.rental.ui/src/com/sogeti/rental/ui/views/AgencyView.java
@@ -0,0 +1,36 @@
+package com.sogeti.rental.ui.views;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+import com.opcoach.training.rental.RentalAgency;
+import com.sogeti.rental.core.RentalCoreActivator;
+
+public class AgencyView extends ViewPart {
+
+	public AgencyView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		TreeViewer tv = new TreeViewer(parent);
+		tv.setContentProvider(new RentalProvider());
+		tv.setLabelProvider(new RentalProvider());
+		Collection<RentalAgency> agencies = new ArrayList<RentalAgency>();
+		agencies.add(RentalCoreActivator.getAgency());
+		tv.setInput(agencies);
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
