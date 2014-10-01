1c4bd52ce33f63cc17a2e1819f6d9b7d8547eec6
LNACHT <LNACHT@LFR002637.corp.capgemini.com>
2014-10-01 13:56:30 +0200

    Ajout de icone

---
+++ ProjectsTrainning/com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalUICstes.java
@@ -0,0 +1,8 @@
+package com.sogeti.rental.ui;
+
+public interface RentalUICstes {
+	public static final String CUSTOMER_NODE = "Customers";
+	public static final String LOCATIONS_NODE = "Locations";
+	public static final String RENTAL_OBJ_NODE = "Objets à louer";
+}
+			
--- ProjectsTrainning/com.sogeti.rental.ui/src/com/sogeti/rental/ui/views/RentalProvider.java
+++ ProjectsTrainning/com.sogeti.rental.ui/src/com/sogeti/rental/ui/views/RentalProvider.java
@@ -2,16 +2,73 @@ package com.sogeti.rental.ui.views;
 
 import java.util.Collection;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
+import com.sogeti.rental.ui.RentalUICstes;
 
 public class RentalProvider extends LabelProvider implements
-		ITreeContentProvider {
+		ITreeContentProvider, RentalUICstes, IColorProvider  {
 
+	@Override
+	public Color getForeground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
+	}
+	
+	class Node {
+		private String lbl_node;
+		private RentalAgency agency;
+		
+		public Node(String lbl_node, RentalAgency agency) {
+			super();
+			this.lbl_node = lbl_node;
+			this.agency = agency;
+		}
+		
+		public boolean hasChildren(Object element) {
+			// TODO Auto-generated method stub
+			return true;
+		}
+		
+		public String getLbl_node() {
+			return lbl_node;
+		}
+
+		public void setLbl_node(String lbl_node) {
+			this.lbl_node = lbl_node;
+		}
+		
+
+		@Override
+		public String toString() {
+			return this.lbl_node;
+		}
+		
+		public Object[] getChildren() {
+			if (this.lbl_node.equals(CUSTOMER_NODE))
+				return agency.getCustomers().toArray();
+			else if (this.lbl_node.equals(RENTAL_OBJ_NODE))
+				return agency.getRentals().toArray();
+			else if (this.lbl_node.equals(LOCATIONS_NODE))
+				return agency.getObjectsToRent().toArray();
+			return null;
+		}
+	}
 	@Override
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		// TODO Auto-generated method stub
@@ -23,6 +80,7 @@ public class RentalProvider extends LabelProvider implements
 		// TODO Auto-generated method stub
 		if(inputElement instanceof Collection)
 			return ((Collection) inputElement).toArray();
+		
 		return null;
 	}
 
@@ -30,7 +88,12 @@ public class RentalProvider extends LabelProvider implements
 	public Object[] getChildren(Object parentElement) {
 		// TODO Auto-generated method stub
 		if (parentElement instanceof RentalAgency)
-			return ((RentalAgency) parentElement).getCustomers().toArray();
+		{
+			RentalAgency a = (RentalAgency) parentElement;
+			return new Node[] {new Node(CUSTOMER_NODE, a), new Node(RENTAL_OBJ_NODE, a), new Node(LOCATIONS_NODE, a)};
+		}
+		else if(parentElement instanceof Node)
+			return ((Node)parentElement).getChildren();
 		return null;
 	}
 
@@ -43,7 +106,7 @@ public class RentalProvider extends LabelProvider implements
 	@Override
 	public boolean hasChildren(Object element) {
 		// TODO Auto-generated method stub
-		if (element instanceof RentalAgency)
+		if ( (element instanceof RentalAgency) || (element instanceof Node))
 			return true;
 		
 		return false;
@@ -55,7 +118,11 @@ public class RentalProvider extends LabelProvider implements
 			return ((RentalAgency)element).getName();
 		else if (element instanceof Customer)
 			return ((Customer)element).getDisplayName();
+		else if (element instanceof RentalObject)
+			return ((RentalObject)element).getName();
 		
-		return "Touchmytralala";
+		return super.getText(element);
 	}
 }
+
+
