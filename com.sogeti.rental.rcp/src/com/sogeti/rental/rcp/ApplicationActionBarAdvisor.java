13834ec23ae7675c86734be8fd08741cb6090384
LNACHT <LNACHT@LFR002637.corp.capgemini.com>
2014-10-03 14:49:49 +0200

    RCP 100

--- com.sogeti.rental.rcp/src/com/sogeti/rental/rcp/ApplicationActionBarAdvisor.java
+++ com.sogeti.rental.rcp/src/com/sogeti/rental/rcp/ApplicationActionBarAdvisor.java
@@ -1,20 +1,82 @@
 package com.sogeti.rental.rcp;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.GroupMarker;
+import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarContributionItem;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.jface.action.IAction;
 
+/**
+ * An action bar advisor is responsible for creating, adding, and disposing of the
+ * actions added to a workbench window. Each window will be populated with
+ * new actions.
+ */
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+    // Actions - important to allocate these only in makeActions, and then use them
+    // in the fill methods.  This ensures that the actions aren't recreated
+    // when fillActionBars is called with FILL_PROXY.
+    private IWorkbenchAction exitAction;
+    private IAction preferencesAction;
+    private IAction preferencesAction_1;
+    
 
     public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
         super(configurer);
     }
+    
+    protected void makeActions(final IWorkbenchWindow window) {
+        // Creates the actions and registers them.
+        // Registering is needed to ensure that key bindings work.
+        // The corresponding commands keybindings are defined in the plugin.xml file.
+        // Registering also provides automatic disposal of the actions when
+        // the window is closed.
 
-    protected void makeActions(IWorkbenchWindow window) {
-    }
-
-    protected void fillMenuBar(IMenuManager menuBar) {
+        exitAction = ActionFactory.QUIT.create(window);
+        register(exitAction);
+        
+    	{
+    		preferencesAction = ActionFactory.PREFERENCES.create(window);
+    		register(preferencesAction);
+    	}
+    	{
+    		preferencesAction_1 = ActionFactory.PREFERENCES.create(window);
+    		register(preferencesAction_1);
+    	}
     }
     
+    protected void fillMenuBar(IMenuManager menuBar) {
+        MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
+        MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP);
+        
+        menuBar.add(fileMenu);
+        // Add a group marker indicating where action set menus will appear.
+        menuBar.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
+        
+        MenuManager menuManager = new MenuManager("&Window");
+        menuBar.add(menuManager);
+        menuManager.add(preferencesAction_1);
+        menuBar.add(helpMenu);
+        
+        // File
+        fileMenu.add(exitAction);
+        
+    }
+    
+    protected void fillCoolBar(ICoolBarManager coolBar) {
+        IToolBarManager toolbar = new ToolBarManager(SWT.FLAT | SWT.RIGHT);
+        coolBar.add(new ToolBarContributionItem(toolbar, "main"));   
+    }
 }
